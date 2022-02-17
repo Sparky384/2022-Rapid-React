751662347a940a438f87022b8089a442d4d2e3d6
Jumper <dcohen@pearsonpkg.com>
2022-02-17 18:40:54 -0500

    got indexer working up to drive team specifications, does not auto drive ball to top eye

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -16,12 +16,9 @@ public class Intake {
     private CANSparkMax stage2Motor; //5
     private CANSparkMax stage3Motor; //11
     //is true during the setup process for shooting so that the ball is in position. bug when holding shooter joystick while intake is going. 
-    private boolean ballInBetween; 
-    //private Controller pilot = new Controller();
     private boolean prevTopEye;
     private boolean prevBottomEye;
     private int ballsInIndex;
-    private boolean shootingOverride;
 
     public Intake() {
         stage1Motor = new CANSparkMax(4, MotorType.kBrushless);
@@ -35,11 +32,9 @@ public class Intake {
 
         bottomPhotoEye = new DigitalInput(0);
         topPhotoEye = new DigitalInput(2);
-        ballInBetween = false;
         prevTopEye = false;
         prevBottomEye = false;
         ballsInIndex = 0;
-        shootingOverride = false;
     }
 
     public void intakeIn() {
@@ -59,49 +54,26 @@ public class Intake {
         stage3Motor.set(0.2);
         SmartDashboard.putBoolean("autoIndex", false);
         SmartDashboard.putBoolean("indexerShoot", true);
-        checkEyes();
-    }
-
-    public void checkEyes()
-    {
-        SmartDashboard.putBoolean("BottomPhotoEyeBlocked", bottomPhotoEye.get());
-        SmartDashboard.putBoolean("TopPhotoEyeBlocked", topPhotoEye.get());
-        SmartDashboard.putBoolean("ballBetween", ballInBetween);
-        SmartDashboard.putNumber("balls in intake", ballsInIndex);
-        SmartDashboard.putBoolean("prev bottom", prevBottomEye);
-        SmartDashboard.putBoolean("prev top", prevTopEye);
-        
-        boolean top = topPhotoEye.get();
-        boolean bottom = bottomPhotoEye.get();
-        if (top || (!bottom && !top && !ballInBetween))
-            ballInBetween = false;
-        else if ((bottom && !top) || (ballInBetween && !top))
-            ballInBetween = true;
-        prevTopEye = top;
     }
 
     public void autoIndex(){
         SmartDashboard.putBoolean("BottomPhotoEyeBlocked", bottomPhotoEye.get());
         SmartDashboard.putBoolean("TopPhotoEyeBlocked", topPhotoEye.get());
-        SmartDashboard.putBoolean("ballBetween", ballInBetween);
         SmartDashboard.putNumber("balls in intake", ballsInIndex);
         SmartDashboard.putBoolean("prev bottom", prevBottomEye);
         SmartDashboard.putBoolean("prev top", prevTopEye);
 
         boolean top = topPhotoEye.get();
         boolean bottom = bottomPhotoEye.get();
-        if (top || (!bottom && !top && !ballInBetween))
+        if (!bottom || top)
         {
             stage2Motor.set(0.0);
             stage3Motor.set(0.0);
-            ballInBetween = false;
         }
-        else if ((bottom && !top) || (ballInBetween && !top))
+        else if (bottom && !top)
         {
-            ballInBetween = true;
             stage2Motor.set(-0.2);
             stage3Motor.set(0.2);
         }
-        prevTopEye = top;
     }    
 }
